739db8de3b5b97dd6203c087f35876ad32fe6a75
Motoaki Tanaka <katachiwotodomete@gmail.com>
2025-05-14 14:15:36 +0000

    feat: add custom theme provider for AWS Amplify UI components

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import '@aws-amplify/ui-react/styles.css';
+import { ThemeProvider } from '@aws-amplify/ui-react';
+
+export const myTheme = {
+  name: 'my-theme',
+  tokens: {
+    colors: {
+      font: {
+        primary: { value: 'red' },
+      },
+    },
+  },
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={myTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
